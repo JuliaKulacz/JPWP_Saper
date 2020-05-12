2919ea729ef1ac8dcdc3243e323cfd2852eda40a
JuliaKulacz <44200333+JuliaKulacz@users.noreply.github.com>
2020-05-12 17:45:44 +0200

    Zadanie 1 - python

---
+++ zad1.py
@@ -0,0 +1,33 @@
+import socket
+import json
+
+HOST = "172.104.229.108"
+PORT = 9001
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    info = json.dumps({"username": "myname"})
+    s.sendall(str.encode(info))
+    data = s.recv(2048)
+
+    msg = data.decode()
+    print('Server reply:', msg)
+
+    json_data = json.loads(msg)
+    print("My session ID:", json_data["session_id"])
+
+    msg = json.dumps({"request": "ALL_ROOMS"})
+    s.sendall(str.encode(msg))
+    response = s.recv(2048).decode()
+    print(response)
+
+    msg = json.dumps({"request": "CREATE_ROOM", "data": "room1"})
+    s.sendall(str.encode(msg))
+    response = s.recv(2048).decode()
+    print(response)
+
+    """
+    Tutaj uzupełnij kod.
+    """
+
+    s.close()
